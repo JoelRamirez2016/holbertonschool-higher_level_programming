e1ba6627d3e47301cc1eedb8b987a159aa74df24
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-05-06 18:55:35 +0000

    fix 0 0x04

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     sq_matrix = []
     for row in matrix:
-        sq_matrix.append(list(map(lambda x: x ** 2, row)));
+        sq_matrix.append(list(map(lambda x: x ** 2, row)))
     return sq_matrix

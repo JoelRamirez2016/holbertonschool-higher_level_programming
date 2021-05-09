bea9c8f7d2085d0440ecc92dbb4d50c37d635a06
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-05-09 21:13:13 +0000

    fix 100 pep8 0x04

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,5 +4,5 @@ def weight_average(my_list=[]):
     div = 0
     for tup in my_list:
         mul += tup[0] * tup[1]
-        div +=  tup[1]
+        div += tup[1]
     return mul / (div if div else 1)

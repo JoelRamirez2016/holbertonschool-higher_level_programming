01519a1ce87c6c71f060373ff00ff6324d3f1837
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-05-18 23:38:34 +0000

    fix 101 0x06

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
 class Square:
     """Square: This class define a Square
 
@@ -17,15 +21,18 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-
-        if type(position) != tuple or type(position[0]) != int or type(position[1]) != int:
+        if not (type(position) == tuple and
+                len(position) == 2 and
+                type(position[0]) == int and
+                type(position[1]) == int and
+                all(v >= 0 for v in position)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
+        self.__size = size
 
     @property
     def position(self):
-        """int: getter of the squeare size"""
+        """int: getter of the squeare position"""
         return self.__position
 
     @property
@@ -33,8 +40,13 @@ class Square:
         """int: getter of the squeare size"""
         return self.__size
 
+    @position.setter
     def position(self, position):
-        if type(position) != tuple or type(position[0]) != int or type(position[1]) != int:
+        if not (type(position) == tuple and
+                len(position) == 2 and
+                type(position[0]) == int and
+                type(position[1]) == int and
+                all(v >= 0 for v in position)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
@@ -51,8 +63,24 @@ class Square:
         """int: area of the squeare based in size attr"""
         return self.__size * self.__size
 
+    def my_print(self):
+        """print the square with the size attr"""
+        if self.__size == 0:
+            print("")
+            return
+        square = "".join(["\n" for c in range(self.__position[1])])
+        for i in range(0, self.__size):
+            square += "".join([" " for c in range(0, self.__position[0])])
+            for j in range(0, self.__size):
+                square += "#"
+            square += "\n"
+        print(square, end="")
+
     def __str__(self):
-        square = ""
+        """print the square with the size attr"""
+        if self.__size == 0:
+            return ""
+        square = "".join(["\n" for c in range(self.__position[1])])
         for i in range(0, self.__size):
             square += "".join([" " for c in range(0, self.__position[0])])
             for j in range(0, self.__size):

07252819fac0aefe51cd39ce8cf475879ca8694f
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-05-19 02:31:48 +0000

    file 103 0x06 up

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class MagicClasa:
+    def __init__(self, radius=0):
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area():
+        return self.__radius ** 2 * math.pi
+
+    def circumference():
+        return 2 * math.pi * self.__radius

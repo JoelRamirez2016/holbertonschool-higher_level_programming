a05c497c3c210b54553017705673d51901ab888f
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-05-21 23:45:51 +0000

    fix 101 slota

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Define LockedClass"""
+
 
 class LockedClass:
+    """Create the LockedClass Class
+
+    Atributes:
+        first_name: string
+
+    """
     __slots__ = 'first_name', 

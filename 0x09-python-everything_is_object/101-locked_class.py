2501dc66955d92e96003692536ce582679a048f7
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-05-21 23:37:40 +0000

    fix 101 slota

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,14 +2,4 @@
 
 
 class LockedClass(object):
-    __isfrozen = False
-
-    def __init__(self):
-        self.first_name = ""
-        self.__isfrozen = True
-
-    def __setattr__(self, key, value):
-        if self.__isfrozen and not hasattr(self, key):
-            raise AttributeError("'{}' object has no attribute '{}'".format(
-                type(self).__name__, key))
-        object.__setattr__(self, key, value)
+    __slots__ = 'first_name', 

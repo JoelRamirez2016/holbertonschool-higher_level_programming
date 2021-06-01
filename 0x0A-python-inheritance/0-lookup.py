d309c36c40df33b3407b661e996362c8eaab83ca
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-01 23:21:33 +0000

    fix doc and pep 0 0x0A

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""This module define the lookup function"""
 
 
 def lookup(obj):
+    """Get the list of available attributes and methods of an object
+    Args:
+        obj (object): object to be checked
+    Return:
+        list of arguments
+    """
     return dir(obj)

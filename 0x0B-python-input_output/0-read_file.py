e2aca61f81a07f8fd3fcad2e87d807244c53374a
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-02 21:59:41 +0000

    add doc 0 0x0B

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""This module define the function read_file"""
 
 
 def read_file(filename=""):
+    """Print text in file"""
     with open(filename) as f:
         print(f.read(), end="")

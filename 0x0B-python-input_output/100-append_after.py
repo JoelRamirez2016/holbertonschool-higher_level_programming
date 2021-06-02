86722d8960828c3858e60a4779b110135a651695
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-02 22:34:30 +0000

    add doc 100 0x0B

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""This module define the append_after function"""
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file, after each
+    line containing a specific string
+    Args:
+        filename (str): file name to be checked
+        search_string (str): text to comparete in each line
+        new_string (str): new line to add
+    """
     with open(filename, "r") as f:
         contents = f.readlines()
 

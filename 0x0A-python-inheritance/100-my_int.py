50bc1a505ad739aa69972500fcbc8673a40609fa
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-02 00:04:22 +0000

    fix 100 0x0A

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""This module define the class MyInt"""
 
 
 class MyInt(int):
+    """This class represents an integer inheriting from int"""
 
     def __eq__(self, other):
+        """Return the eq comparison of Myint != int"""
         return self.real != other
 
     def __ne__(self, other):
+        """Return the eq comparison of Myint == int"""
         return self.real == other

2c13c8afdb525b219ef3a414a4fca7c239f0a49a
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-02 23:17:04 +0000

    restore 7 0x0B

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""7-add_item
-This script reads a JSON file and saves to it the arguments from the terminal.
-"""
+"""Adds all arguments to a Python list, and then save them to a file"""
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    filename = "add_item.json"
-    try:
-        ls = load_from_json_file(filename)
-    except Exception:
-        ls = []
-    ls.extend(args)
-    save_to_json_file(ls, filename)
+
+try:
+
+    l = load_from_json_file("add_item.json")
+
+except:
+
+    l = []
+
+for arg in sys.argv[1:]:
+    l.append(arg)
+
+save_to_json_file(l, "add_item.json")
+

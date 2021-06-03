a7b6573a3e461edea50b96fdc3c070bccecf4e59
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-03 00:16:18 +0000

    fix docs and main condition

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,30 +5,37 @@ prints those statistics since the beginning"""
 import sys
 
 
-def prints_statistics(statuses, size):
-    print("File size: {}".format(size))
+if __name__ == '__main__':
 
-    for status, quantity in sorted(statuses.items()):
-        if quantity > 0:
-            print("{}: {}".format(status, quantity))
+    def prints_statistics(statuses, size):
+        """shows stored counts and total size
+        Args:
+            statuses (dict): dict of status to print
+            size (int): cumulative size by requests
+        """
+        print("File size: {}".format(size))
 
-valid_status = {
-    200: 0, 301: 0, 400: 0, 401: 0,
-    403: 0, 404: 0, 405: 0, 500: 0
-}
+        for status, quantity in sorted(statuses.items()):
+            if quantity > 0:
+                print("{}: {}".format(status, quantity))
 
-status = 0
-size = 0
-count = 0
+    valid_status = {
+        200: 0, 301: 0, 400: 0, 401: 0,
+        403: 0, 404: 0, 405: 0, 500: 0
+    }
 
-try:
-    for line in sys.stdin:
-        count += 1
-        statistics = [int(s) for s in line.split() if s.isdigit()]
-        size += statistics[1]
-        valid_status[statistics[0]] += 1
+    status = 0
+    size = 0
+    count = 0
 
-        if count % 10 == 0:
-            prints_statistics(valid_status, size)
-except KeyboardInterrupt:
-    prints_statistics(valid_status, size)
+    try:
+        for line in sys.stdin:
+            count += 1
+            statistics = [int(s) for s in line.split() if s.isdigit()]
+            size += statistics[1]
+            valid_status[statistics[0]] += 1
+
+            if count % 10 == 0:
+                prints_statistics(valid_status, size)
+    except KeyboardInterrupt:
+        prints_statistics(valid_status, size)

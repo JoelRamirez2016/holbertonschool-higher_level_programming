a07518f8b6b3e0f93e757ee59bb82f9fc43e8a88
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-06-03 00:36:38 +0000

    fix 101 0x0B Ctrl D

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,26 +5,26 @@ prints those statistics since the beginning"""
 import sys
 
 
+
+def prints_statistics(statuses, size):
+    """shows stored counts and total size
+    Args:
+        statuses (dict): dict of status to print
+        size (int): cumulative size by requests
+    """
+    print("File size: {}".format(size))
+
+    for status, quantity in sorted(statuses.items()):
+        if quantity > 0:
+            print("{}: {}".format(status, quantity))
+
 if __name__ == '__main__':
-
-    def prints_statistics(statuses, size):
-        """shows stored counts and total size
-        Args:
-            statuses (dict): dict of status to print
-            size (int): cumulative size by requests
-        """
-        print("File size: {}".format(size))
-
-        for status, quantity in sorted(statuses.items()):
-            if quantity > 0:
-                print("{}: {}".format(status, quantity))
 
     valid_status = {
         200: 0, 301: 0, 400: 0, 401: 0,
         403: 0, 404: 0, 405: 0, 500: 0
     }
 
-    status = 0
     size = 0
     count = 0
 
@@ -41,6 +41,7 @@ if __name__ == '__main__':
 
             if count % 10 == 0:
                 prints_statistics(valid_status, size)
+        prints_statistics(valid_status, size)
     except KeyboardInterrupt:
         prints_statistics(valid_status, size)
         raise

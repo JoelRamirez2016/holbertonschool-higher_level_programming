dd3f96cb9ced11a8a0263dec3b673e45d9f14fc6
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-08-03 19:55:31 +0000

    fix 6 0x12

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-// coment
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const el of arr) console.log(el);
+// Prints each value from an array.
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+msg.forEach(val => console.log(val));

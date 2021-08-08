2bf86e8a008022eac5ac90e56dace155a0c61dca
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-08-08 17:18:44 +0000

    fix 3 docs only

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+takes in arguments and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
+
+"""
+
 import MySQLdb
 import sys
 
@@ -10,7 +17,8 @@ if __name__ == "__main__":
                            db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4],))
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE %s", (sys.argv[4],))
 
     for row in cur.fetchall():
         print(row)

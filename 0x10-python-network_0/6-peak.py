97657c33b04319080de6d151e37e6c748f84e1de
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-08-19 21:07:47 +0000

    fix 6 0x10, move intial conditions

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,13 +4,13 @@
 
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
-    mid_i = int(len(list_of_integers) / 2)
-
     if list_of_integers == []:
         return None
 
     if len(list_of_integers) <= 2:
         return max(list_of_integers)
+
+    mid_i = int(len(list_of_integers) / 2)
 
     if list_of_integers[mid_i] < list_of_integers[mid_i - 1]:
         return find_peak(list_of_integers[:mid_i])

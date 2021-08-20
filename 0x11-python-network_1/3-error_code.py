ea75df8b5b0c735fa36729907ef68592a231f3ce
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-08-20 17:30:03 +0000

    fix 3,4 0x11

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,11 +3,11 @@
 body of the response (decoded in utf-8)"""
 import urllib.request
 import sys
-from urllib.error import HTTPError
+import urllib.error
 
 if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
-    except HTTPError as e:
+    except urllib.error.HTTPError as e:
         print("Error code: ", e.code)
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
 
-    print("Body response:\n",
-          "\t- type: {}\n".format(type(r)),
+    print("Body response:\n" +
+          "\t- type: {}\n".format(type(r)) +
           "\t- content: {}".format(r.text))

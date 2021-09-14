4615b8bad2d1820bc273c1cfeef1f83e31336d4a
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-09-14 18:03:52 +0000

    fix 0 0x14

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', function (err, content) {
-  const data = content || err;
+fs.readFile(process.argv[2], function (err, content) {
+  const data = err || content.toString('utf-8');
   console.log(data);
 });

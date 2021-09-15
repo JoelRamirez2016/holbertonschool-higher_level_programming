1ad9b5d7e3212ece5e51eb7492ff91487393473c
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-09-15 22:56:43 +0000

    fix 4 parameters

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,9 +2,7 @@
 
 const request = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/films/';
-
-request.get(url, function (error, response, body) {
+request.get(process.argv[2], function (error, response, body) {
   const data = error || JSON.parse(body).results.reduce((acum, val) => {
     if (val.characters.find((character) => character.includes('18'))) {
       return acum + 1;
